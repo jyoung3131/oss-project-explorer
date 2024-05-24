8f3557af67b05e09966daf75ce2985e8b4781ddc
Nate Kim <natekim@Nates-MacBook-Air.local>
2024-05-24 12:00:05 -0700

    Changed about section email to ospo-directors

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -5,7 +5,7 @@ const AboutSection = () => {
         <div className="mx-auto max-w-5xl py-4 px-6 mt-4 mb-2 bg-gray-100 rounded-lg shadow-md">
             <h2 className="text-xl font-semibold mb-2">About This Resource</h2>
             <p>This page provides an overview of Georgia Tech open source research projects curated by the Georgia Tech Open Source Project Office (OSPO). Georgia Tech PIs and researchers can submit their work via the form below, and all submissions are reviewed via a PR process before being posted.</p>
-            <p>If you have any questions about this resource, please reach out to us via email at <a href="mailto:ospo-team@gatech.edu" className="text-gtgold hover:text-gtgoldlight">ospo-team@gatech.edu</a>.</p>
+            <p>If you have any questions about this resource, please reach out to us via email at <a href="mailto:ospo-directors@gatech.edu" className="text-gtgold hover:text-gtgoldlight">ospo-directors@gatech.edu</a>.</p>
         </div>
     )
 }
